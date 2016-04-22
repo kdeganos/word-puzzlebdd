0b544d746d2a2ca0ad7b7ae37f502038b9cbc0f1
Kevin Deganos <kdeganos-09@sandiego.edu>
2016-04-22 09:54:23 -0700

    Replaces vowels in a string withe dash symbol. No UI yet.

---
+++ src/main/java/WordPuzzle.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class WordPuzzle{
+
+  public String makePuzzle (String phrase) {
+    String vowels = "aeiou";
+    char[] toPuzzle = phrase.toCharArray();
+
+    for (int index = 0; index < phrase.length(); index++){
+      if (vowels.indexOf(toPuzzle[index]) >= 0) {
+        toPuzzle[index] = '-';
+      }
+    }
+
+    String puzzled = new String(toPuzzle);
+    return puzzled;
+   }
+}
---
+++ src/test/java/WordPuzzleTest.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.fluentlenium.adapter.FluentTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WordPuzzleTest extends FluentTest{
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+   public WebDriver getDefaultDriver() {
+     return webDriver;
+   }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+ // @Test
+ // public void rootTest() {
+ //   goTo("http://localhost:4567/");
+ //    fill("#???").with("?");
+ //    submit(".btn");
+ //    assertThat(pageSource()).contains("what you want to be found related to your method");
+ // }
+
+ @Test
+ public void WordPuzzle_returnsAStringAsIsWhenNoVowelsArePresent_NoChange() {
+   WordPuzzle testWordPuzzle = new WordPuzzle();
+   assertEquals("znmrp", testWordPuzzle.makePuzzle("znmrp"));
+ }
+
+  @Test
+  public void WordPuzzle_replacesVowelsWithDashes_StringWithDashesForVowels() {
+    WordPuzzle testWordPuzzle = new WordPuzzle();
+    assertEquals("B-l--v- y-- c-n -nd y--'r- h-lfw-y th-r-. Th--d-r- R--s-v-lt", testWordPuzzle.makePuzzle("Believe you can and you're halfway there. Theodore Roosevelt"));
+  }
+}
